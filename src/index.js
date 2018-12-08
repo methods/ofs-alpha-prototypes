261ae87f1dcd859e080b4fbbca311d84129bf6b2
Tom Ridd <twridd@gmail.com>
2018-12-08 09:01:23 +0000

    React app with router for multiple pages, sass support and the basic govuk pattern library

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Homepage from "./scenes/Homepage"
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./styles/govuk/all.scss"
+
+const App = (props) => {
+    return (
+      <Router>
+        <Switch>
+            <Route exact path="/" component={Homepage} />
+        </Switch>
+      </Router>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
