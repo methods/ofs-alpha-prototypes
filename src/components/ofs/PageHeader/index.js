b10a64db43ad08ac3947d82f9d1ab851ea9354be
Tom Ridd <twridd@gmail.com>
2018-12-14 15:16:12 +0000

    Created component for page headlines (including like-share links)

--- src/components/ofs/PageHeader/index.js
+++ src/components/ofs/PageHeader/index.js
@@ -1,13 +1,29 @@
 import React from  'react';
 import './styles.scss';
+import LikeShare from '../LikeShare';
 
 export default (props) => {
   return (
     <div className="ofs-page-header">
         <div className="ofs-width-container">
             <div className="ofs-grid-row">
+                <div className="ofs-grid-column-three-quarters">
+                    <div className="ofs-page-header__title">
+                      <div className="ofs-page-header__title-image" ><img src={props.imgUrl} alt="" /></div>
+                      <div className="ofs-page-header__title-label"><h2 className="ofs-heading-l">{props.title}</h2></div>
+                    </div>
+                </div>
+                <div className="ofs-grid-column-one-quarter">
+                    <div className="ofs-page-header__like-share">
+                        <LikeShare />
+                    </div>
+                </div>
+            </div>
+            <div className="ofs-grid-row">
                 <div className="ofs-grid-column-full">
-                    <h2 className="ofs-heading-l">{props.title}</h2>
+                    <div className="ofs-body ofs-!-font-weight-bold">
+                      {props.content}
+                    </div>
                 </div>
             </div>
         </div>
