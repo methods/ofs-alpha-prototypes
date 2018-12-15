90286347d4419e5300f9b442c23341503484b4c5
Tom Ridd <twridd@gmail.com>
2018-12-15 01:03:29 +0000

    Added youtube video in the video version

--- src/scenes/Homepage/components/VideoJumbotron/index.js
+++ src/scenes/Homepage/components/VideoJumbotron/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './styles.scss'
-
+import LikeShare from '../../../../components/ofs/LikeShare';
 export default (props) => {
+    const videoId = "38nLc4MEZ84"
+    const url = `https://www.youtube.com/embed/${videoId}`
     return (
         <div className="video-jumbotron">
             <div className="ofs-width-container">
                 <div className="ofs-grid-row">
-                    <div className="ofs-grid-column-full">
-                        <img src="/assets/jumbotron/video.png" alt="Placeholder for a video introduction to the site" />
+                    <div className="ofs-grid-column-two-thirds">
+
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe className="video-jumbotron__video embed-responsive-item" src={url}></iframe>
+                        </div>
+                    </div>
+                    <div className="ofs-grid-column-one-third">
+                        <div className="video-jumbotron__text">
+                            <p className="ofs-body">When you have so many interests for your career choosing the right college course is super tricky! If you have passions, hobbies or just interests.</p>
+                            <p className="ofs-body ofs-!-font-weight-bold">In this video students share their advice on how you can focus on the right path for you</p>
+                            <div><LikeShare /></div>
+                        </div>
                     </div>
                 </div>
             </div>
